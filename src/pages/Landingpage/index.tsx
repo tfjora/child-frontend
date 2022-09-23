be3d169616b5bf78327512fab3a8fea76beca2c8
thomasFjorstad <thomas.fjorstad@bouvet.no>
2022-09-23 11:33:03 +0200

    Added eslint

--- src/pages/Landingpage/index.tsx
+++ src/pages/Landingpage/index.tsx
@@ -1,33 +1,34 @@
-import { useParams } from "react-router-dom";
-import Header from "../../components/Header";
-import { LandingPageTabs } from "../../_models/Tabs";
-import Person from "./Person";
-import PersonDetail from "./PersonDetail";
-import Quote from "./Quotes";
-import { useStyles } from "./style";
+import { useParams } from 'react-router-dom';
+
+import { LandingPageTabs } from '../../_models/Tabs';
+import Header from '../../components/Header';
+import Person from './Person';
+import PersonDetail from './PersonDetail';
+import Quote from './Quotes';
+import { useStyles } from './style';
 
 export default function LandingPage() {
-  const { tab } = useParams();
-  const styles = useStyles();
+    const { tab } = useParams();
+    const styles = useStyles();
 
-  const renderTabs = () => {
-    switch (tab) {
-      case LandingPageTabs.person:
-        return <Person />;
-      case LandingPageTabs.personDetails:
-        return <PersonDetail />;
-      case LandingPageTabs.quotes:
-        return <Quote />;
+    const renderTabs = () => {
+        switch (tab) {
+            case LandingPageTabs.person:
+                return <Person />;
+            case LandingPageTabs.personDetails:
+                return <PersonDetail />;
+            case LandingPageTabs.quotes:
+                return <Quote />;
 
-      default:
-        return <div></div>;
-    }
-  };
+            default:
+                return <div></div>;
+        }
+    };
 
-  return (
-    <div className={styles.container}>
-      <Header />
-      {renderTabs()}
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <Header />
+            {renderTabs()}
+        </div>
+    );
 }
